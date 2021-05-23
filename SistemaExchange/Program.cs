bd145b104463b8092ded85595f197417cb7fa290
roger-rocha <rogerrochabnusc@gmail.com>
2021-05-23 18:18:59 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaExchange/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
